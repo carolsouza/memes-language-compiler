0b7c845a8944b99bd364435f9f1022983d3e0256
carolsouza <caah.souza@live.com>
2022-11-06 20:23:20 -0300

    comunicação com back-end e ajustes na doc

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  compileCode(code: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/compiler`, {
+       code
+    })
+  }
 }
